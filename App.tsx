0976488e2a5576d893f33d9d30ff091136616ebe
Rubanraaj <rubanraaj14@gmail.com>
2025-07-15 01:47:21 +0530

    Add redux provider

--- App.tsx
+++ App.tsx
@@ -6,28 +6,52 @@
  */
 
 import { createStackNavigator } from '@react-navigation/stack';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { StackNavigationProp } from '@react-navigation/stack';
 import { HomeScreen } from './src/screens/HomeScreen';
 import { FavoritesScreen } from './src/screens/FavoritesScreen';
+import { Provider } from 'react-redux';
+import { store } from './src/store';
 
 export type RootStackParamList = {
-  Home: undefined;
+  Home: { location: string } | undefined;
   Favorites: undefined;
 }
 
 const Stack = createStackNavigator<RootStackParamList>();
 
+const FavoritesButton = ({ navigation }: { navigation: StackNavigationProp<RootStackParamList> }) => (
+  <TouchableOpacity onPress={() => navigation.push('Favorites')}>
+    <Text style={styles.favoritesButton}>Favorites</Text>
+  </TouchableOpacity>
+);
+
+const renderFavoritesButton = (navigation: StackNavigationProp<RootStackParamList>) => (
+  <FavoritesButton navigation={navigation} />
+);
+
 const RootStack = () => {
   return (
+    <Provider store={store}>
     <Stack.Navigator initialRouteName='Home'>
-      <Stack.Screen name="Home" component={HomeScreen} />
-      <Stack.Screen 
-        name="Favorites" 
+      <Stack.Screen
+        name="Home"
+        component={HomeScreen}
+        initialParams={{ location: 'Karur' }}
+        options={({ navigation }) => ({
+          title: 'Weather App',
+          headerTitleAlign: 'center',
+          headerRight: () => renderFavoritesButton(navigation),
+        })}
+      />
+      <Stack.Screen
+        name="Favorites"
         component={FavoritesScreen}
-        options={{ title: 'Favorites' }} 
+        options={{ title: 'Favorites' }}
       />
     </Stack.Navigator>
+    </Provider>
   );
 }
 
@@ -42,6 +66,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  favoritesButton: {
+    marginRight: 20,
+    color: 'blue'
+  }
 });
 
 export default App;
